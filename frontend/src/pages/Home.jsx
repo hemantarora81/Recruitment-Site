bd6f31e5cbf488ab24db3a0962dee5f159ef49f0
Hemant Arora <arorahemant75@gmail.com>
2024-09-02 18:12:56 +0530

    Loader

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -115,9 +115,9 @@ const Home = () => {
               Join us today to streamline your recruitment process.
             </p>
             <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-white backdrop-blur-lg px-6 py-2 text-base font-semibold text-red-500 transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-white-600/50 border border-white/20">
-              <a className="text-lg" href="/hr-form">
+              <span className="text-lg"   onClick={() => navigate("/hr-form")}>
                 Sign Up Now For Ideal candidates
-              </a>
+              </span>
               <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
                 <div className="relative h-full w-10 bg-white/30"></div>
               </div>
